800a8efae92d2f77e0e7b7cc8b672c9d54815ab1
Alameen007 <alaminabdulkareem@gmail.com>
2017-04-08 12:44:51 +0100

    countdown awesomeness

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,11 @@ module.exports = {
     root: __dirname,
     alias: {
       Main: 'app/components/Main.jsx',
-      Navigation: 'app/components/Navigation.jsx',
       applicationStyles: 'app/styles/app.scss',
+      Navigation: 'app/components/Navigation.jsx',
       Timer: 'app/components/Timer.jsx',
       Countdown: 'app/components/Countdown.jsx',
+      Clock: 'app/components/Clock.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
